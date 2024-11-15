32980c4636c44bf393c5d2c1a398cd077de1ed04
Bryan Snyder <bryan.snyder@delaware.gov>
2024-11-15 00:33:39 -0500

    tick added

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
 
     # Game loop
     while True:
@@ -15,9 +19,12 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+            
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
