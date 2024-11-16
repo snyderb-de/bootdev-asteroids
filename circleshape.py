e4e459504827ef2bae2cf9bcf57e8f541a04a896
Bryan Snyder <bryan.snyder@delaware.gov>
2024-11-16 00:03:36 -0500

    player drawn as expected

--- circleshape.py
+++ circleshape.py
@@ -15,7 +15,7 @@ class CircleShape(pygame.sprite.Sprite):
 
     def draw(self, screen):
         # sub-classes must override
-        pass
+        pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
 
     def update(self, dt):
         # sub-classes must override
--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 import constants
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -11,7 +12,7 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     # Game loop
     while True:
@@ -22,6 +23,10 @@ def main():
             
 
         screen.fill((0, 0, 0))
+        
+        player.update(dt)
+        player.draw(screen)
+        
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
