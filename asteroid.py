beb91c3f439af01a043750d1e2d7c7f44fddd969
Bryan Snyder <bryan.snyder@delaware.gov>
2024-11-23 13:06:20 -0500

    asteroids are splitting as expected

--- asteroid.py
+++ asteroid.py
@@ -1,14 +1,35 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+import random
 
 class Asteroid(CircleShape):
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
+    def __init__(self, x, y, radius, velocity=None):
+        super().__init__(x, y, radius,)
+        self.velocity = velocity if velocity else pygame.Vector2(0, 0)
 
     def draw(self, screen):
         pygame.draw.circle(screen, (255, 255, 255), self.position, self.radius, 2)
 
     def update(self, dt):
         self.position += self.velocity * dt
+    
+    def split(self):
+        # create two new asteroids based on ASTEROID_MIN_RADIUS  
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            # remove the current asteroid
+            self.kill()
+        else:
+            angle = random.uniform(20, 50)
+            # use rotate method on asteroid's velocity to create 2 new vectors, rotated by angle and -angle.
+            velocity1 = self.velocity.rotate(angle) * 1.5
+            velocity2 = self.velocity.rotate(-angle) * 1.5
+            # compute the new radius for the 2 new asteroids
+            new_radius = self.radius / 2
+            # create the 2 new asteroids at the same position as the current asteroid, the first asteroid's velocity is multiplyed by 1.5
+            Asteroid(self.position.x, self.position.y, new_radius, velocity1)
+            Asteroid(self.position.x, self.position.y, new_radius, velocity2)
+        
+            # remove the current asteroid
+            self.kill()
 
--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def main():
         for asteroid in asteroids_group:
             for shot in shots_group:
                 if isinstance(asteroid, Asteroid) and isinstance(shot, Shot) and asteroid.check_collision(shot):
-                    asteroid.kill()  # Remove the asteroid from the game
+                    asteroid.split()  # Remove the asteroid from the game
                     shot.kill()  # Remove the shot from the game
 
 
@@ -70,12 +70,6 @@ def main():
         for sprite in draw_group:
             sprite.draw(screen)
         
-        #Check for collisions
-        for asteroid in asteroids_group:
-            if isinstance(asteroid, Asteroid) and player.check_collision(asteroid):
-                print("Game Over!")
-                pygame.quit()
-                return
 
         pygame.display.flip()
 
